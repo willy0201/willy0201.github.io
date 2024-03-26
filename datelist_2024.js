c6e2ef2c04adea7c2bb659f65621bec50ea1de54
willy0201 <125789952+willy0201@users.noreply.github.com>
2024-03-26 23:59:13 +0800

    Update datelist_2024.js

--- datelist_2024.js
+++ datelist_2024.js
@@ -1,5 +1,6 @@
 datelist_2024 = '<ul class="list" style="width:12%;"> \
                     <li>日期</li>\
+                    <a href="../pages/20240326.html" id="20240326">2024-03-26</a><br>\
                     <a href="../pages/20240315.html" id="20240315">2024-03-15</a><br>\
                     <a href="../pages/20240227.html" id="20240227">2024-02-27</a><br>\
                     <a href="../pages/20240117.html" id="20240117">2024-01-17</a><br>\
