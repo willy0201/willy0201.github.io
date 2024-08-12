67e5fd535a5dbc855141eaa16ff13d130d8039db
willy0201 <h24099069@gs.ncku.edu.tw>
2024-08-12 23:15:37 +0800

    new

---
+++ datelist_2023.js
@@ -0,0 +1,26 @@
+datelist_2023 = '<ul class="list" style="width:12%;"> \
+                    <li>日期</li>\
+                    <a href="../pages/20231031.html" id="20231031">2023-10-31</a><br>\
+                    <a href="../pages/20231026.html" id="20231026">2023-10-26</a><br>\
+                    <a href="../pages/20231023.html" id="20231023">2023-10-23</a><br>\
+                    <a href="../pages/20231018.html" id="20231018">2023-10-18</a><br>\
+                    <a href="../pages/20231004.html" id="20231004">2023-10-04</a><br>\
+                    <a href="../pages/20230921.html" id="20230921">2023-09-21</a><br>\
+                    <a href="../pages/20230918.html" id="20230918">2023-09-18</a><br>\
+                    <a href="../pages/20230825.html" id="20230825">2023-08-25</a><br>\
+                    <a href="../pages/20230814.html" id="20230814">2023-08-14</a><br>\
+                    <a href="../pages/20230810.html" id="20230810">2023-08-10</a><br>\
+                    <a href="../pages/20230802.html" id="20230802">2023-08-02</a><br>\
+                    <a href="../pages/20230731.html" id="20230731">2023-07-31</a><br>\
+                    <a href="../pages/20230718.html" id="20230718">2023-07-18</a><br>\
+                    <a href="../pages/20230706.html" id="20230706">2023-07-06</a><br>\
+                    <a href="../pages/20230627.html" id="20230627">2023-06-27</a><br>\
+                    <a href="../pages/20230626.html" id="20230626">2023-06-26</a><br>\
+                    <a href="../pages/20230608.html" id="20230608">2023-06-08</a><br>\
+                    <a href="../pages/20230425.html" id="20230425">2023-04-25</a><br>\
+                    <a href="../pages/20230310.html" id="20230310">2023-03-10</a><br>\
+                    <a href="../pages/20230215.html" id="20230215">2023-02-15</a><br>\
+                    <a href="../pages/20230206.html" id="20230206">2023-02-06</a>\
+                </ul>';
+
+document.write(datelist_2023);
---
+++ datelist_2024.js
@@ -0,0 +1,35 @@
+var Date_List1 = new String("20240116,20240117,20240227,20240315,20240326,20240415,20240416,20240419,20240425,20240604,20240624,20240712,20240717,20240718,20240719,20240722,20240726,20240802,20240805,20240808");
+var Date_List2 = new String("2024-01-16,2024-01-17,2024-02-27,2024-03-15,2024-03-26,2024-04-15,2024-04-16,2024-04-19,2024-04-25,2024-06-04,2024-06-24,2024-07-12,2024-07-17,2024-07-18,2024-07-19,2024-07-22,2024-07-26,2024-08-02,2024-08-05,2024-08-08");
+
+
+var Date_List1 = Date_List1.split(",");
+var Date_List2 = Date_List2.split(",");
+
+/*
+datelist_2024 = '<ul class="list" style="width:12%;"> \
+                <select onchange="location.href=this.options[this.selectedIndex].value">\
+                <option>日期</option>\
+                <option value="../pages/20240425.html" id="20240425">2024-04-25</option>\
+                <option value="../pages/20240419.html" id="20240419">2024-04-19</option>\
+                <option value="../pages/20240416.html" id="20240416">2024-04-16</option>\
+                <option value="../pages/20240415.html" id="20240415">2024-04-15</option>\
+                <option value="../pages/20240326.html" id="20240326">2024-03-26</option>\
+                <option value="../pages/20240315.html" id="20240315">2024-03-15</option>\
+                <option value="../pages/20240227.html" id="20240227">2024-02-27</option>\
+                <option value="../pages/20240117.html" id="20240117">2024-01-17</option>\
+                <option value="../pages/20240116.html" id="20240116">2024-01-16</option>\
+                </select>\
+                </ul>'
+
+document.write(datelist_2024);
+*/
+
+/*
+document.write('<ul class="list" style="width:12%;"> \
+                <select onchange="location.href=this.options[this.selectedIndex].value">\
+                <option>日期</option>');
+for(var i = 0; i < Date_List1.length; i++) {
+    document.write("<option value='../pages/"+Date_List1[i]+".html' id='"+Date_List1[i]+"'>"+Date_List2[i]+"</option>");
+}
+document.write("</select></ul>")
+*/
